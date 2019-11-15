81a43c177b17ba6620353616449d2d0be8b40d51
adversarialpolicies <50158213+adversarialpolicies@users.noreply.github.com>
2019-11-14 21:05:09 -0800

    Update from yarn to db7fb136cd2cacbd5dd66e9839deace4a5d182bf

---
+++ precache-manifest.4966c397f2a680fe6f802804d02b963b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06d83c244e97133a67958dce669f9b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469bb9f9d4a401fc48a9",
+    "url": "/static/css/main.dd67d482.chunk.css"
+  },
+  {
+    "revision": "5ebd97d3db23274ae973",
+    "url": "/static/js/2.7846d340.chunk.js"
+  },
+  {
+    "revision": "469bb9f9d4a401fc48a9",
+    "url": "/static/js/main.eadd2a02.chunk.js"
+  },
+  {
+    "revision": "e3747d6afb87ab83e32b",
+    "url": "/static/js/runtime-main.4022d934.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c717f0b71000770c071ec92288a72150.js"
+  "/precache-manifest.4966c397f2a680fe6f802804d02b963b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
